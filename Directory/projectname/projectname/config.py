15c83194c009e901d601017cb08eacf213536a9a
SarahCoding <sarah.atwi@gmail.com>
2020-01-15 11:40:37 -0500

    initial commit

---
+++ Directory/projectname/projectname/config.py
@@ -0,0 +1,15 @@
+# config.py
+
+from pathlib import Path  # pathlib is seriously awesome!
+
+data_dir = Path('/path/to/some/logical/parent/dir')
+data_path = data_dir / 'my_file.csv'  # use feather files if possible!!!
+
+customer_db_url = 'sql:///customer/db/url'
+purchases_db_url = 'sql:///purchases/db/url'
+
+# notebook.ipynb
+from projectname.config import data_path
+import pandas as pd
+
+df = pd.read_csv(data_path)  # clean!
